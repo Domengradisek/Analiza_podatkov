f03681f749fc24a55fbb861d561c50de2ba0d4d2
Domen <domko.grad@gmail.com>
2020-11-01 13:21:12 +0100

    Doda sem več podatkov v vzorec

--- poberi_stran.py
+++ poberi_stran.py
@@ -3,7 +3,7 @@ import re
 import orodja
 
 
-STEVILO_STRANI = 10
+STEVILO_STRANI = 2
 
 vzorec = (
     r'<tr data-playerid="(?P<id>\d*?)">' # zajamemo ID igralca
@@ -15,6 +15,17 @@ vzorec = (
     r'(?P<kakovost>\d{2})' # kakovost
     r'</span><span class="badge badge-dark rating .*?">'
     r'(?P<potencial>\d{2})' # potencial
+    r'</span></td><td data-title="Name">.*?'
+    r'<td data-title="Preferred Positions".*?title="'
+    r'(?P<polozaj_1>.*?)" class="link-position">'
+    r'<span .*?</span>'
+    r'(</a><a href.*? title="(?P<polozaj_2>.*?)" class="link-position"><span .*?</span>)?'
+    r'(</a><a href.*? title="(?P<polozaj_3>.*?)" class="link-position"><span .*?</span>)?'
+    r'</a></td><td data-title="Age">'
+    r'(?P<starost>\d{2})'
+    r'</td><td data-title="Hits">(?P<iskanja>\d*?)'
+    r'</td><td data-title="Team">.*?'
+    r'title="(?P<klub>.*?) FIFA 21".*?'
 )
 
 najdeni_igralci = 0
@@ -26,7 +37,7 @@ for stran in range(STEVILO_STRANI):
     orodja.shrani_spletno_stran(url, datoteka)
     vsebina = orodja.vsebina_datoteke(datoteka)
     for zadetek in re.finditer(vzorec, vsebina):
-        #print(zadetek.groupdict())
+        print(zadetek.groupdict())
         najdeni_igralci += 1
 
 print(najdeni_igralci)
